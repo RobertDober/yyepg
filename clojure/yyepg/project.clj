cedf8203e103f120eaf3c5e0b9a5c0cc91c7cdff
RobertDober <robert.dober@gmail.com>
2017-11-02 19:10:58 +0100

    moved clojure implementation -> clojure/yyepg

---
+++ clojure/yyepg/project.clj
@@ -0,0 +1,7 @@
+(defproject yyepg "0.1.0-SNAPSHOT"
+  :description "A parser for My Shiny Little Markup Lang (yyepg)"
+  :url "http://example.com/FIXME"
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [midje "1.8.3"]])
---
+++ clojure/yyepg/test/yyepg/core_test.clj
@@ -0,0 +1,10 @@
+(ns yyepg.core-test
+  (:require [clojure.test :refer :all]
+            [midje.sweet :refer :all]
+            [yyepg.core :refer :all]))
+
+(facts "about equity"
+  (fact "1 equals 1"
+        1 => 1))
+
+
